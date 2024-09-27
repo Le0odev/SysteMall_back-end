a903c13c4b7d8e28760ed3548c1205d2c7217a1c
Leonardo Santos <95249193+Le0odev@users.noreply.github.com>
2024-09-27 10:22:43 -0300

    Update SysteMallBackEndApplication.java

--- src/main/java/com/example/SysteMall_backend/SysteMallBackEndApplication.java
+++ src/main/java/com/example/SysteMall_backend/SysteMallBackEndApplication.java
@@ -3,11 +3,20 @@ package com.example.SysteMall_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
-public class 	SysteMallBackEndApplication {
+public class SysteMallBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SysteMallBackEndApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        // Define o fuso horário com -3 horas de diferença em relação ao padrão UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT-3"));
+        System.out.println("Fuso horário definido para GMT-3");
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SysteMallBackEndApplication.class, args);
+    }
 }
